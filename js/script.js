f311e76df76d0e3715a38e611c222882955e9350
Will Moritz <will@crowdfavorite.com>
2013-05-20 10:09:58 -0600

    good video in

--- js/script.js
+++ js/script.js
@@ -1,17 +1,24 @@
 var APP = {
 	init: function() {
 		this.navToggle();
+		this.vidToggle();
 	},
 
 	navToggle: function() {
 		$('.nav-toggle').on('click', function() {
 			$(this).toggleClass('off');
-			$('.nav-overlay').toggleClass('off');
+			$('.nav-slider').toggleClass('off');
 		});
 
 		$('.nav-list a').on('click', function() {
 			$('.nav-toggle').toggleClass('off');
-			$('.nav-overlay').toggleClass('off');
+			$('.nav-slider').toggleClass('off');
+		});
+	},
+
+	vidToggle: function() {
+		$('.vid-toggle').on('click', function() {
+			$(this).parent().toggleClass('vid-on');
 		});
 	}
 };
