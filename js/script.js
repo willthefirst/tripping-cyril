557d76e3e61bdc3cabad36c6672d0561ac013a86
Will Moritz <will@crowdfavorite.com>
2013-08-21 10:19:51 -0600

    vids are now playing with js api

--- js/script.js
+++ js/script.js
@@ -10,13 +10,11 @@ var APP = {
 		$('.nav-toggle').on('click', function() {
 			$(this).toggleClass('off');
 			$('.nav-slider').toggleClass('off');
-            console.log('off');
 		});
 
 		$('.nav-list a').on('click', function() {
 			$('.nav-toggle').toggleClass('off');
 			$('.nav-slider').toggleClass('off');
-            console.log('on');
 		});
 	},
 
